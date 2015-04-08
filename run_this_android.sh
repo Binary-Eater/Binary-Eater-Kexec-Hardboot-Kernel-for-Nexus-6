64e71bc8bdad3dd9b2f3b74d25a5d92540dec015
TKOBabu <rahulrameshbabu16@mittymonarch.com>
2015-04-08 09:09:15 -0700

    Added Build Tools

---
+++ run_this_android.sh
@@ -0,0 +1,7 @@
+export CC=$(pwd)/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.7/bin/arm-eabi-
+
+export ARCH=arm
+export SUBARCH=arm
+
+export PATH=$PATH:$(pwd)/mkbootimg_tools
