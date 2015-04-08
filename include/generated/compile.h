f3ba91d3cf8bc6683b26420b0dbe60e66505ccfa
TKOBabu <rahulrameshbabu16@mittymonarch.com>
2015-04-08 08:42:35 -0700

    Kexec Port for Stock 5.0.1 5.0 Kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Mon Dec 29 18:27:06 PST 2014"
+#define LINUX_COMPILE_BY "rahul"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.40-shamu-kexec-hardboot_@author_binary-eater-g15096bd-dirty"
